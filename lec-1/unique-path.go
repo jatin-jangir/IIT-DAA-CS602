4bca7182fd1ad5dd400e6d1c0fb7cb018bb71731
jatin-jangir <79351203+jatin-jangir@users.noreply.github.com>
2024-08-01 22:53:00 +0530

    Create unique-path.go

---
+++ lec-1/unique-path.go
@@ -0,0 +1,7 @@
+`
+Definition: A directed graph G￼=(￼V , E￼) is said to be unique-path graph if 
+For each pair ￼u,v in V ￼, there is at most one path from ￼u to v￼.
+Aim:  Design an efficient algorithm to determine if ￼ is unique-path graph.
+
+A solution must be of O(n*n)￼ time 
+`
