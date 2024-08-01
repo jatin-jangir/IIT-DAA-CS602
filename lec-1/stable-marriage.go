fbee82742cab5bb3816c22003f218bb155adf797
jatin-jangir <79351203+jatin-jangir@users.noreply.github.com>
2024-08-01 22:50:05 +0530

    Create stable-marriage.go

---
+++ lec-1/stable-marriage.go
@@ -0,0 +1,10 @@
+`
+￼M : set of ￼men 
+￼W : set of ￼women 
+ For each man m in M￼, 
+		 M[i]￼: a preference list of all women
+ For each woman w in W￼,
+		W[i]￼: a preference list of all men
+
+Aim: To compute a stable marriage.
+`
